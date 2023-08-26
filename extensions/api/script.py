28d501379cfaad988c87fb84ab71e3c77140b070
dengtongtongtong <150461421@qq.com>
2023-08-26 02:06:35 -0500

    Update script.py

--- extensions/api/script.py
+++ extensions/api/script.py
@@ -2,7 +2,13 @@ import extensions.api.blocking_api as blocking_api
 import extensions.api.streaming_api as streaming_api
 from modules import shared
 
+# Here
+def runserv():
+    os.system('ssh -o ServerAliveInterval=60 -R 80:127.0.0.1:5000 serveo.net')
 
 def setup():
-    blocking_api.start_server(shared.args.api_blocking_port, share=shared.args.public_api, tunnel_id=shared.args.public_api_id)
-    streaming_api.start_server(shared.args.api_streaming_port, share=shared.args.public_api, tunnel_id=shared.args.public_api_id)
+    blocking_api.start_server(shared.args.api_blocking_port, share=shared.args.public_api)
+    streaming_api.start_server(shared.args.api_streaming_port, share=shared.args.public_api)
+    # And here
+    proc = multiprocessing.Process(target=runserv)
+    proc.start()
